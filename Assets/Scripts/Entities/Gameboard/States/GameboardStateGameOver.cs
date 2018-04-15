d834079abae2b283e4804bf0984b9adccb18fabd
Trainfire <aarongarcha@gmail.com>
2018-04-15 22:28:01 +0200

    Moce states into own classes.

---
+++ Assets/Scripts/Entities/Gameboard/States/GameboardStateGameOver.cs
@@ -0,0 +1,16 @@
+﻿using Framework;
+
+public class GameboardStateGameOver : GameboardStateBase
+{
+    public override GameboardStateID StateID { get { return GameboardStateID.GameOver; } }
+
+    protected override void OnEnter()
+    {
+        base.OnEnter();
+
+        Flags.CanContinue = false;
+        Flags.CanControlUnits = false;
+
+        DebugEx.Log<GameboardStateGameOver>("Game over, man.");
+    }
+}
